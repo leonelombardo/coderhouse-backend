0889fb84e83fee78b3597cd3cd24d8f7843eca3f
Leonel <leonelombardo1@gmail.com>
2022-12-26 18:43:39 -0300

    cleaning the code up

--- challenge-2/product_manager.js
+++ challenge-2/product_manager.js
@@ -2,18 +2,19 @@ const fs = require("fs")
 
 class ProductManager {
     constructor(path){
-        this.products = []
         this.path = path
     }
 
     addProduct = async (object) => {
         const { title, description, price, thumbnail, code, stock } = object
+        const hasUndefinedKeys = [title, description, price, thumbnail, code, stock].some(key => key == null || key == undefined)
 
-        if([title, description, price, thumbnail, code, stock].some(x => x == null || x == null)){
-            console.log("You should complete all fields.")
+        if(hasUndefinedKeys){
+            console.error("You should complete all fields.")
             return
         }else{
-            const isCodeRepeated = this.products.find(product => product.code == object.code)
+            const products = await this.getProducts()
+            const isCodeRepeated = products.find(product => product.code == object.code)
 
             if(isCodeRepeated){
                 console.error("This code is already used.")
@@ -30,7 +31,7 @@ class ProductManager {
         if(fs.existsSync(this.path)){
             const response = await fs.promises.readFile(this.path, "utf8")
             const data = JSON.parse(response)
-    
+
             return data
         }else{
             return []
@@ -42,7 +43,6 @@ class ProductManager {
         const foundProduct = products.find(product => product.id === id)
 
         if(foundProduct){
-            console.log(foundProduct)
             return foundProduct
         }else{
             return console.error("Product not found.")
@@ -64,7 +64,6 @@ class ProductManager {
         }
 
         const updatedProducts = products.map(x => x.id === updatedProduct.id ? updatedProduct :  x)
-        console.log(`Product ${updatedProduct.title} ${updatedProduct.description} updated.`)
 
         await fs.promises.writeFile(this.path, JSON.stringify(updatedProducts, null , "\t"))
     }
@@ -77,4 +76,4 @@ class ProductManager {
     }
 }
 
-const manager = new ProductManager("./users.json")
+const manager = new ProductManager("./products.json")
