2b2956b8668cefb63bd8c118130f9f39c8c7cb4d
Leonel <leonelombardo1@gmail.com>
2023-02-12 18:16:48 -0300

    feature: sockets.io added to server

--- practice-1/src/app.js
+++ practice-1/src/app.js
@@ -1,4 +1,11 @@
+import { Server } from "socket.io"
 import { app } from "./index.js"
 import { PORT } from "./config.js"
 
-app.listen(PORT, () => console.log(`Server running at port ${PORT}`))
+const httpServer = app.listen(PORT, () => console.log(`Server running at port ${PORT}`))
+
+export const io = new Server(httpServer)
+
+io.on("connection", socket => {
+    console.log(`Socket ${socket.id} connected.`)
+})
