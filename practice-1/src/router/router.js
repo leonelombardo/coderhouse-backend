3d76b6f04b985be793da11f16398109de55f0d70
Leonel <leonelombardo1@gmail.com>
2023-02-13 17:02:22 -0300

    feature: socket added to app.js + chats controller added to router

--- practice-1/src/router/router.js
+++ practice-1/src/router/router.js
@@ -1,11 +1,13 @@
-import { realtimeProductsController } from "../controllers/realtimeproducts.controller.js"
 import { productsController } from "../controllers/products.controller.js"
 import { cartsController } from "../controllers/carts.controller.js"
 import { messagesController } from "../controllers/messages.controller.js"
+import { chatsController } from "../controllers/chats.controller.js"
+import { realtimeProductsController } from "../controllers/realtimeProducts.controller.js"
 
 export const router = (app) => {
-    app.use("/", realtimeProductsController)
     app.use("/products", productsController)
     app.use("/carts", cartsController)
     app.use("/messages", messagesController)
+    app.use("/chat", chatsController)
+    app.use("/", realtimeProductsController)
 }
