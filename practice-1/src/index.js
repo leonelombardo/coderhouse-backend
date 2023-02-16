c440c1c55c88d09614dbb54af0191cc68782ff53
Leonel Lombardo <leonelombardo1@gmail.com>
2023-02-16 12:43:08 -0300

    feature: errorHandler middleware added to app

--- practice-1/src/index.js
+++ practice-1/src/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose"
 import handlebars from "express-handlebars"
 
 import { router } from "./router/router.js"
+import { errorHandler } from "./middlewares/errorHandler.js"
+
 import { MONGODB_CLUSTER, MONGODB_PASSWORD, MONGODB_USER } from "./db/config.db.js"
 import { __dirname } from "./config.js"
 
@@ -21,3 +23,5 @@ mongoose.set("strictQuery", false)
 mongoose.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}${MONGODB_CLUSTER}/ecommerce?retryWrites=true&w=majority`, error => error && console.error(error))
 
 router(app)
+
+app.use(errorHandler)
