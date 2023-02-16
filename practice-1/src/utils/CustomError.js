9e60d04131429c9e83b1cbd99f8e4523953bd5d7
Leonel Lombardo <leonelombardo1@gmail.com>
2023-02-16 12:37:42 -0300

    feature: CustomError

---
+++ practice-1/src/utils/CustomError.js
@@ -0,0 +1,8 @@
+export class CustomError extends Error{
+    constructor({ message, status, ok, response }){
+        super(message),
+        this.status = status,
+        this.ok = ok,
+        this.response = response
+    }
+}
