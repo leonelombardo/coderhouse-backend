b69b7f93618125c1ae402df7ce40283860d74e71
Leonel Lombardo <leonelombardo1@gmail.com>
2023-02-16 12:38:41 -0300

    feature: middleware => errorHandler

---
+++ practice-1/src/middlewares/errorHandler.js
@@ -0,0 +1,7 @@
+export const errorHandler = (error, req, res, next) => {
+    const status = error.status || 500
+    const ok = error.ok || false
+    const response = error.response || "Internal server error."
+
+    res.status(status).json({ status, ok, response })
+}
