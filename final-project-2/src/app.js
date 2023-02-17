975b60670adc2e79d6fd2ddd22f24cfac18a2fdd
Leonel Lombardo <leonelombardo1@gmail.com>
2023-02-17 16:10:07 -0300

    first commit: packages + server config

---
+++ final-project-2/src/app.js
@@ -0,0 +1,31 @@
+import { Server } from "socket.io"
+import { app } from "./index.js"
+import { PORT } from "./config/server.config.js"
+import { MessageManager } from "./dao/managers/mongodb_managers/message.manager.js"
+
+const httpServer = app.listen(PORT, () => console.log(`Server running at port ${PORT}`))
+
+export const io = new Server(httpServer)
+
+const messageManager = new MessageManager()
+
+io.on("connection", async (socket) => {
+    console.log(`Socket ${socket.id} connected.`)
+    
+    const messages = await messageManager.getMessages()
+
+    socket.emit("load-messages", messages.response)
+
+    socket.on("email", data => {
+        socket.broadcast.emit("user-connected", data)
+    })
+
+    socket.on("message", async (data) => {
+        const { email, message } = data
+
+        const response = await messageManager.createMessage({ user: email, message })
+        const messages = await messageManager.getMessages()
+
+        io.emit("new-message", messages.response)
+    })
+})
---
+++ final-project-2/src/index.js
@@ -0,0 +1,27 @@
+import express from "express"
+import mongoose from "mongoose"
+import handlebars from "express-handlebars"
+
+import { router } from "./router/router.js"
+import { errorHandler } from "./middlewares/errorHandler.js"
+
+import { MONGODB_CLUSTER, MONGODB_PASSWORD, MONGODB_USER } from "./config/db.config.js"
+import { __dirname } from "./config/server.config.js"
+
+export const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + "/public"))
+
+app.engine("handlebars", handlebars.engine())
+
+app.set("views", __dirname + "/views")
+
+mongoose.set("strictQuery", false)
+
+mongoose.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}${MONGODB_CLUSTER}/ecommerce?retryWrites=true&w=majority`, error => error && console.error(error))
+
+router(app)
+
+app.use(errorHandler)
