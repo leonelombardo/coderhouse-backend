635fd1864cfa32fec5c843b79992a82d62b93f17
Leonel Lombardo <leonelombardo1@gmail.com>
2023-02-17 16:11:38 -0300

    feature: router

---
+++ final-project-2/src/router/router.js
@@ -0,0 +1,13 @@
+import { productsController } from "../controllers/products.controller.js"
+import { cartsController } from "../controllers/carts.controller.js"
+import { messagesController } from "../controllers/messages.controller.js"
+import { chatsController } from "../controllers/chats.controller.js"
+import { realtimeProductsController } from "../controllers/realtimeProducts.controller.js"
+
+export const router = (app) => {
+    app.use("/api/products", productsController)
+    app.use("/api/carts", cartsController)
+    app.use("/api/messages", messagesController)
+    app.use("/chat", chatsController)
+    app.use("/", realtimeProductsController)
+}
