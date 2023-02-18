a598c0ad09f4f170a42bb213232e383186c4d359
Leonel Lombardo <leonelombardo1@gmail.com>
2023-02-18 16:44:36 -0300

    update: __dirname moved from server.config.js to index.js

--- final-project-2/src/config/server.config.js
+++ final-project-2/src/config/server.config.js
@@ -1,10 +1,5 @@
 import { config } from "dotenv"
-import path from 'path'
-import { fileURLToPath } from 'url'
 
 config()
-
-const __filename = fileURLToPath(import.meta.url);
-export const __dirname = path.dirname(__filename);
 
 export const PORT = process.env.PORT || 3000
--- final-project-2/src/index.js
+++ final-project-2/src/index.js
@@ -1,12 +1,16 @@
 import express from "express"
 import mongoose from "mongoose"
 import handlebars from "express-handlebars"
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 import { router } from "./router/router.js"
 import { errorHandler } from "./middlewares/errorHandler.js"
 
 import { MONGODB_CLUSTER, MONGODB_PASSWORD, MONGODB_USER } from "./config/db.config.js"
-import { __dirname } from "./config/server.config.js"
+
+const __filename = fileURLToPath(import.meta.url)
+export const __dirname = path.dirname(__filename)
 
 export const app = express()
 
@@ -15,6 +19,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname + "/public"))
 
 app.engine("handlebars", handlebars.engine())
+
 
 app.set("views", __dirname + "/views")
 
