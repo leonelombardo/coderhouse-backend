f8feadd8043764569495008990585770fa816b17
Leonel Lombardo <leonelombardo1@gmail.com>
2023-02-21 20:31:35 -0300

    update: populate added to cart schema

--- final-project-2/src/dao/models/carts.model.js
+++ final-project-2/src/dao/models/carts.model.js
@@ -3,7 +3,18 @@ import mongoose from "mongoose"
 const cartsCollection = "carts"
 
 const cartSchema = new mongoose.Schema({
-    products: Array
+    products: {
+        type: [
+            {
+                id: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "products" 
+                }
+            }
+        ]
+    }
 })
+
+cartSchema.pre("find", function() { this.populate("products.id") })
 
 export const cartModel = mongoose.model(cartsCollection, cartSchema)
