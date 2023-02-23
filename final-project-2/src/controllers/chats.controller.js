7063014a7ecbd96362f8cf7be1b7137de3241941
Leonel Lombardo <leonelombardo1@gmail.com>
2023-02-23 18:39:39 -0300

    delete: all chat files removed (didn't mean to be in this project)

--- final-project-2/src/controllers/chats.controller.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express"
-
-export const chatsController = Router()
-
-chatsController.get("/", async (req, res) => {
-    res.status(200).render("chat.handlebars", { style: "chat.css", title: "Chat" })
-})
