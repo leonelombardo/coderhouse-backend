501ebf3d49334fccc486c1269f4d3048a1eb4b14
Leonel Lombardo <leonelombardo1@gmail.com>
2023-02-23 18:39:39 -0300

    delete: all chat files removed (didn't mean to be in this project)

--- final-project-2/src/router/router.js
+++ final-project-2/src/router/router.js
@@ -1,7 +1,6 @@
 import { productsController } from "../controllers/products.controller.js"
 import { cartsController } from "../controllers/carts.controller.js"
 import { messagesController } from "../controllers/messages.controller.js"
-import { chatsController } from "../controllers/chats.controller.js"
 import { productsViewController } from "../controllers/productsView.controller.js"
 import { cartsViewController } from "../controllers/cartsView.controller.js"
 import { realtimeProductsController } from "../controllers/realtimeProducts.controller.js"
@@ -10,7 +9,6 @@ export const router = (app) => {
     app.use("/api/products", productsController)
     app.use("/api/carts", cartsController)
     app.use("/api/messages", messagesController)
-    app.use("/chat", chatsController)
     app.use("/products", productsViewController)
     app.use("/carts", cartsViewController)
     app.use("/", realtimeProductsController)
